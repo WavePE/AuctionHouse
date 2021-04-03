37e284d98062de0f70c49b81ef6eaad46e16afe8
xxAROX <arox.email.0@gmail.com>
2021-04-03 22:01:06 +0200

    Added Utils::decodeItem & Utils::encodeItem

---
+++ src/main/java/club/wavepe/xxarox/utils/Utils.java
@@ -0,0 +1,21 @@
+package club.wavepe.xxarox.utils;
+
+import cn.nukkit.item.Item;
+import cn.nukkit.nbt.NBTIO;
+
+import java.io.IOException;
+import java.nio.ByteOrder;
+import java.util.Base64;
+
+public class Utils {
+    private static ByteOrder byteOrder = ByteOrder.BIG_ENDIAN;
+
+
+    static String encodeItem(Item item) throws IOException {
+        return Base64.getEncoder().encodeToString(NBTIO.write(NBTIO.putItemHelper(item), byteOrder));
+    }
+
+    static Item decodeItem(String buffer) throws IOException {
+        return NBTIO.getItemHelper(NBTIO.read(Base64.getDecoder().decode(buffer), byteOrder));
+    }
+}
