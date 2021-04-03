60cd3b658f48a5ff194c8147118bea5241682443
xxAROX <arox.email.0@gmail.com>
2021-04-03 20:24:18 +0200

    Added provider interface

--- src/main/java/club/wavepe/xxarox/AuctionHouse.java
+++ src/main/java/club/wavepe/xxarox/AuctionHouse.java
@@ -2,10 +2,13 @@ package club.wavepe.xxarox;
 
 import club.wavepe.xxarox.generic.EndedListing;
 import club.wavepe.xxarox.generic.Listing;
+import club.wavepe.xxarox.provider.JsonProvider;
+import club.wavepe.xxarox.provider.Provider;
 import cn.nukkit.plugin.PluginBase;
 
 public class AuctionHouse extends PluginBase {
     protected static AuctionHouse instance;
+    protected Provider provider;
 
     private Listing[] cache = new Listing[0];
     private EndedListing[] waitForCollect = new EndedListing[0];
@@ -14,17 +17,34 @@ public class AuctionHouse extends PluginBase {
     public void onLoad() {
         instance = this;
         saveDefaultConfig();
+
+        provider = getProviderFromString(getConfig().getString("provider", "json"));
+
+        provider.init(this);
     }
 
     @Override
     public void onEnable() {
+        provider.prepare();
+        provider.load(this);
     }
 
     @Override
     public void onDisable() {
+        provider.shutdown();
     }
 
     public static AuctionHouse getInstance() {
         return instance;
+    }
+
+    private Provider getProviderFromString(String providerType) {
+        switch (providerType.toLowerCase()) {
+            case "json":
+            case "js": {
+                return new JsonProvider();
+            }
+        }
+        throw new RuntimeException("Provider '" + providerType + "' not found!");
     }
 }
---
+++ src/main/java/club/wavepe/xxarox/provider/Provider.java
@@ -0,0 +1,24 @@
+package club.wavepe.xxarox.provider;
+
+import club.wavepe.xxarox.AuctionHouse;
+import club.wavepe.xxarox.generic.Bid;
+import club.wavepe.xxarox.generic.EndedListing;
+import club.wavepe.xxarox.generic.Listing;
+
+public interface Provider {
+    void init(AuctionHouse plugin);
+
+    void prepare();
+
+    void load(AuctionHouse plugin);
+
+    void onCreate(Listing listing);
+
+    void onBid(Bid bid);
+
+    void onEnd(Listing listing);
+
+    void onCollect(EndedListing listing);
+
+    void shutdown();
+}
